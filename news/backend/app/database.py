81ab3669dbc0593dc578c72637b4d27b4667d324
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2025-12-17 18:30:37 -0500

    Update database.py

--- news/backend/app/database.py
+++ news/backend/app/database.py
@@ -8,6 +8,6 @@ load_dotenv()
 DB_URL = os.getenv("DB_URL")
 
 engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocmmmit=False)
 Base = declarative_base()
 
